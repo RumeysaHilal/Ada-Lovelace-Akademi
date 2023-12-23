47581863748467298fd37d4c9a9fee94839c0845
RumeysaHilal <rumeysahilalsevinc@gmail.com>
2023-12-23 10:51:47 +0300

    python concepts

---
+++ Week-5/my_module.py
@@ -0,0 +1,21 @@
+def my_function():
+    print("hello!")
+
+if __name__ == "__main__":
+    my_function()
+
+
+
+
+# import pandas as pd
+
+# df = pd.DataFrame([1,2])
+
+# print("this is my module")
+# print(pd.__name__)
+
+# import datetime
+# from datetime import timezone
+
+# print(timezone.__name__)
+# print(datetime.__name__)
